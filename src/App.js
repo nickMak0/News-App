ba88b704c62beb76a0babad0ba8a398d971922d8
nickMak0 <nikhilmakwana41@gmail.com>
2022-01-24 20:35:36 +0530

    new project

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
-import About from './components/About.js';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+// import About from './components/About.js';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route
+// } from "react-router-dom";
 
 
 // import About from './components/About';
@@ -46,20 +46,20 @@ function App() {
   }
   return (
     <>
-    <Router>
+    {/* <Router> */}
     <Navbar title="TextUtils" AboutText="About" mode={mode} toggleMode={toggleMode}/>
     <Alert alert={alert}/>
     <div className="container my-3">
-    <Switch>
+    {/* <Switch>
           <Route exact path="/about">
             <About />
           </Route>
-          <Route exact path="/">
+          <Route exact path="/"> */}
             <TextForm showAlert={showAlert}heading="Enter the text to analyze below" mode={mode}/>
-          </Route>
-    </Switch>
+          {/* </Route>
+    </Switch> */}
     </div>
-    </Router>
+    {/* </Router> */}
     
 
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Link} from 'react-router-dom'
+// import {Link} from 'react-router-dom'
 
 export default function Navbar(props) {
     return (
         <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
         <div className="container-fluid">
-        <Link class="navbar-brand" to="/">
-      <img src="/text.png" alt="logo" width="30" height="30"/>
-    </Link>
-          <Link className="navbar-brand" to="/">{props.title}</Link>
+        {/* <Link class="navbar-brand" to="/"> */}
+        {/* <a className="navbar-brand" href="#">
+      <img src="/1.png" alt="" width="30" height="24" class="d-inline-block align-text-top"/>
+    </a> */}
+          <a className="navbar-brand" href="#">{props.title}</a>
+          {/* <Link className="navbar-brand" to="/">{props.title}</Link> */}
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link  className="nav-link active" aria-current="page" to="/">Home</Link>
+                {/* <Link  className="nav-link active" aria-current="page" to="/">Home</Link> */}
+                <a  className="nav-link active" aria-current="page" href="/">Home</a>
               </li>
-              <li className="nav-item">
+              {/* <li className="nav-item">
                 <Link  className="nav-link" to="/about">{props.AboutText}</Link>
               </li>
-              
+               */}
               
             </ul>
             {/* <form className="d-flex">
